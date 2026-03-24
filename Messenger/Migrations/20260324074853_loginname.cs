efa970d471002ab76d74be293417618c7c629b2e
alferovapolina <alfyorova.polina@gmail.com>
2026-03-24 11:38:04 +0300

    Дополнение

---
+++ Messenger/Migrations/20260324074853_loginname.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Messenger.Migrations
+{
+    /// <inheritdoc />
+    public partial class loginname : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_users_nickname",
+                table: "users");
+
+            migrationBuilder.RenameColumn(
+                name: "nickname",
+                table: "users",
+                newName: "name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "users",
+                newName: "nickname");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_nickname",
+                table: "users",
+                column: "nickname",
+                unique: true);
+        }
+    }
+}
